08bbb634a40fa8a9e904cf460f673dc121b586ac
sejaljain123 <sejaljain2043@gmail.com>
2021-08-23 00:20:05 +0530

    changed socket connection

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: 'https://shunt-frontend.vercel.app/',
     methods: ['GET', 'POST'],
     credentials: true,
   },
